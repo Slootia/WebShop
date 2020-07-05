10710f1e2b3ebbc2d76f23684904d507afcb96d5
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-05 16:44:30 +0500

    Заготовка сервиса

---
+++ WebShop/Infrastructure/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Infrastructure.Interfaces;
+using WebShop.Models;
+
+namespace WebShop.Infrastructure.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Employee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
