f3043125e028b37830e26a6b52ad6d53323cae28
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-05 16:42:11 +0500

    Добавление интерфейса для работы с сотрудниками

---
+++ WebShop/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Models;
+
+namespace WebShop.Infrastructure.Interfaces
+{
+    interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+
+        Employee GetById(int id);
+
+        int Add(Employee employee);
+
+        void Employee(Employee employee);
+
+        bool Delete(int id);
+
+        void SaveChanges();
+    }
+}
