c4a107259fd9c7979ab45a2e3a9b7936a46fa2db
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-05 16:47:27 +0500

    Перенос тестовых данных

--- WebShop/Controllers/EmployeesController.cs
+++ WebShop/Controllers/EmployeesController.cs
@@ -3,49 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebShop.Infrastructure.Data;
 using WebShop.Models;
 
 namespace WebShop.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _employees = new List<Employee>
-        {
-            new Employee()
-            {
-                Id = 1,
-                Surname = "Иванов",
-                Name = "Иван",
-                Patronymic = "Иванович",
-                Age = 25
-            },
-            new Employee()
-            {
-                Id = 2,
-                Surname = "Петров",
-                Name = "Петр",
-                Patronymic = "Петрович",
-                Age = 52
-            },
-            new Employee()
-            {
-                Id = 3,
-                Surname = "Сидоров",
-                Name = "Алексей",
-                Patronymic = "Семенович",
-                Age = 23
-            },
-            new Employee()
-            {
-                Id = 4,
-                Surname = "Основин",
-                Name = "Александр",
-                Patronymic = "Алексеевич",
-                Age = 38
-            }
-        };
-
-
+        private static readonly List<Employee> _employees = TestData.Empolyees;
         public IActionResult Index() => View(_employees);
 
         public IActionResult Details(int id)
---
+++ WebShop/Infrastructure/Data/TestData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Models;
+
+namespace WebShop.Infrastructure.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Empolyees { get; } = new List<Employee>
+        {
+            new Employee()
+            {
+                Id = 1,
+                Surname = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Age = 25
+            },
+            new Employee()
+            {
+                Id = 2,
+                Surname = "Петров",
+                Name = "Петр",
+                Patronymic = "Петрович",
+                Age = 52
+            },
+            new Employee()
+            {
+                Id = 3,
+                Surname = "Сидоров",
+                Name = "Алексей",
+                Patronymic = "Семенович",
+                Age = 23
+            },
+            new Employee()
+            {
+                Id = 4,
+                Surname = "Основин",
+                Name = "Александр",
+                Patronymic = "Алексеевич",
+                Age = 38
+            }
+        };
+    }
+}
