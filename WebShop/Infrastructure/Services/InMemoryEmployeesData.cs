1c75a2940815cb7f2be87f9fd59e8c7a61b512ae
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-05 16:57:51 +0500

    Реализация интерфейса управления сотрудниками

--- WebShop/Infrastructure/Services/InMemoryEmployeesData.cs
+++ WebShop/Infrastructure/Services/InMemoryEmployeesData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebShop.Infrastructure.Data;
 using WebShop.Infrastructure.Interfaces;
 using WebShop.Models;
 
@@ -9,34 +10,51 @@ namespace WebShop.Infrastructure.Services
 {
     public class InMemoryEmployeesData : IEmployeesData
     {
+
+        private static readonly List<Employee> _employees = 
+            TestData.Empolyees;
+
+        public IEnumerable<Employee> Get() => _employees;
+
+        public Employee GetById(int id) => 
+            _employees.FirstOrDefault(e => e.Id == id);
+
         public int Add(Employee employee)
         {
-            throw new NotImplementedException();
-        }
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
+            if (_employees.Contains(employee))
+                return employee.Id;
+
+            employee.Id = _employees.Count == 0 ? 1 : _employees.Max(e => e.Id) + 1;
+            _employees.Add(employee);
+
+            return employee.Id;
         }
 
         public void Employee(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+            
+            if (_employees.Contains(employee)) return;
+
+            var dbEmployee = GetById(employee.Id);
+            if (dbEmployee is null) return;
+
+            dbEmployee.Name = employee.Name;
+            dbEmployee.Surname = employee.Surname;
+            dbEmployee.Patronymic = employee.Patronymic;
+            dbEmployee.Age = employee.Age;
+
         }
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Employee GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Delete(int id) =>
+            _employees.RemoveAll(e => e.Id == id) > 0;
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
         }
     }
 }
