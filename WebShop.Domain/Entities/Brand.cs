bc2075726743890aefbb90d015ec843a4aebfab3
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-09 16:39:21 +0500

    Определение сущностей "Бренд" и "Секция"

---
+++ WebShop.Domain/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShop.Domain.Entities.Base;
+using WebShop.Domain.Entities.Base.Interfaces;
+
+namespace WebShop.Domain.Entities
+{
+    class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
