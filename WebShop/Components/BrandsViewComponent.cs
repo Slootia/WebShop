7c8588303fedb4b0a9e81b0de3123f2b3cfe5af4
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-09 17:27:50 +0500

    Визуальный компонент списка брендов

---
+++ WebShop/Components/BrandsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Infrastructure.Interfaces;
+using WebShop.ViewModels;
+
+namespace WebShop.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public BrandsViewComponent(IProductData productData) => _productData = productData;
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() =>
+            _productData.GetBrands()
+                .Select(brand => new BrandViewModel
+                {
+                    Id = brand.Id,
+                    Name = brand.Name,
+                    Order = brand.Order
+                }).OrderBy(brand => brand.Order);
+    }
+}
