cf2f8c8f0cb5f9e13388edbac071e3ee9b0eb5a6
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-09 16:36:54 +0500

    Базовые абстрактные классы сущностей

---
+++ WebShop.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,9 @@
+﻿using WebShop.Domain.Entities.Base.Interfaces;
+
+namespace WebShop.Domain.Entities.Base
+{
+    public abstract class Entity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WebShop.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,9 @@
+﻿using WebShop.Domain.Entities.Base.Interfaces;
+
+namespace WebShop.Domain.Entities.Base
+{
+    public abstract class NamedEntity : Entity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
