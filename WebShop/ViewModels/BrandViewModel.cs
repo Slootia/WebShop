6872571dfc18fded966852d901c8d5caf64f1dc9
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-09 16:55:36 +0500

    Модели-представления для бренда и для секции

---
+++ WebShop/ViewModels/BrandViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebShop.Domain.Entities.Base.Interfaces;
+
+namespace WebShop.ViewModels
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
