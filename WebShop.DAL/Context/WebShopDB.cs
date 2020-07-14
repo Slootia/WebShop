a025448baff34161ac88961e04b7eae8f2a58ce3
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-14 14:10:33 +0500

    Заготовка контекста

---
+++ WebShop.DAL/Context/WebShopDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShop.DAL.Context
+{
+    public class WebShopDB : DbContext
+    {
+        public WebShopDB(DbContextOptions<WebShopDB> options) : base(options)
+        {
+            
+        }
+    }
+}
