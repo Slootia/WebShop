75b3852438a03a7d0088d88e238309a006460a37
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-14 14:02:26 +0500

    Добавление анотаций данных к сущностям

--- WebShop.Domain/Entities/Product.cs
+++ WebShop.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using WebShop.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebShop.Domain.Entities.Base;
 using WebShop.Domain.Entities.Base.Interfaces;
 
 namespace WebShop.Domain.Entities
@@ -8,8 +9,16 @@ namespace WebShop.Domain.Entities
         public int Order { get; set; }
 
         public int SectionId { get; set; }
+        [ForeignKey(nameof(SectionId))]
+        public virtual Section Section { get; set; }
+
         public int? BrandId { get; set; }
+        [ForeignKey(nameof(BrandId))]
+        public virtual Brand Brand { get; set; }
+
         public string ImageUrl { get; set; }
+
+        [Column(TypeName = "decimal(18,2")]
         public decimal Price { get; set; }
     }
 }
--- WebShop.Domain/Entities/Section.cs
+++ WebShop.Domain/Entities/Section.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using WebShop.Domain.Entities.Base;
 using WebShop.Domain.Entities.Base.Interfaces;
 
 namespace WebShop.Domain.Entities
 {
-    [Table("ProductBrands")]
-    public class Brand : NamedEntity, IOrderedEntity
+    [Table("ProductSections")]
+    public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]
+        public virtual Section ParentSection { get; set; }
 
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
