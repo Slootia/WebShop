76d694cecdf814ff4d834a00dc4f7716f38505b1
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-14 14:12:28 +0500

    Контекст БД

--- WebShop.DAL/Context/WebShopDB.cs
+++ WebShop.DAL/Context/WebShopDB.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using WebShop.Domain.Entities;
 
 namespace WebShop.DAL.Context
 {
     public class WebShopDB : DbContext
     {
-        public WebShopDB(DbContextOptions<WebShopDB> options) : base(options)
-        {
-            
-        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+
+        public WebShopDB(DbContextOptions<WebShopDB> options) : base(options) { }
     }
 }
