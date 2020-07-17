b78628137f0cb57b772d44d4d2f8bc706adfb2b6
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-17 15:04:35 +0500

    Добавление модели пользователя и роли в систему

---
+++ WebShop.Domain/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebShop.Domain.Identity
+{
+    class User : IdentityUser
+    {
+        public const string Administrator = "Administrator";
+        public const string DefaultAdminPassword = "AdPass";
+
+        public string Description { get; set; }
+    }
+}
