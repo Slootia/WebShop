c727a94a67c30c82cfd722f6f662fa98a7edd1c3
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-17 15:22:57 +0500

    Заготовка контроллера аккаунта

---
+++ WebShop/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Domain.Identity;
+
+namespace WebShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register() => View();
+        public IActionResult Login() => View();
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+        public IActionResult AccessDenied() => View();
+    }
+}
