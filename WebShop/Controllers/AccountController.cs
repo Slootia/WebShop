d05117bccc8dbdbc7c55b54f0ad56357f67980d3
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-17 15:57:51 +0500

    Логика работы аккаунта

--- WebShop/Controllers/AccountController.cs
+++ WebShop/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration;
 using WebShop.Domain.Identity;
 using WebShop.ViewModels.Identity;
 
@@ -16,19 +19,77 @@ namespace WebShop.Controllers
             _signInManager = signInManager;
         }
 
+        #region Регистрация нового пользователя
+
         public IActionResult Register() => View(new RegisterUserViewModel());
 
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
                 return View(model);
+
+            var user = new User
+            {
+                UserName = model.UserName
+            };
+
+            var registrationResult = await _userManager.CreateAsync(user, model.Password);
+
+            if (registrationResult.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registrationResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+                return View(model);
+            }
 
             return RedirectToAction("Index", "Home");
         }
 
-        public IActionResult Login() => View();
-        public IActionResult Logout() => RedirectToAction("Index", "Home");
+        #endregion
+
+        #region Вход пользователя в систему
+
+        public IActionResult Login(string returnUrl) => View(new LoginViewModel {ReturnUrl = returnUrl});
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+
+            var loginResult = await _signInManager.PasswordSignInAsync(
+                model.UserName,
+                model.Password,
+                model.RememberMe,
+                false);
+
+            if (loginResult.Succeeded)
+            {
+                if (Url.IsLocalUrl(model.ReturnUrl))
+                    return Redirect(model.ReturnUrl);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя или пароль");
+
+            return View(model);
+        }
+
+        #endregion
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
         public IActionResult AccessDenied() => View();
     }
 }
