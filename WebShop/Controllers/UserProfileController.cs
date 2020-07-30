77c224faa1548dd95b0e4f662308f0c26dab3ce4
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-31 00:39:13 +0500

    Заготовка профиля пользователя

---
+++ WebShop/Controllers/UserProfileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
