50185214ef1c84d889d982b1ac233972344fb23d
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-30 14:17:46 +0500

    Ограничение доступа, исправление разметки

--- WebShop/Areas/Admin/Controllers/HomeController.cs
+++ WebShop/Areas/Admin/Controllers/HomeController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebShop.Domain.Identity;
 
 namespace WebShop.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
