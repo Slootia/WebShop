945b358ed03f35ea14c63852e36ac1489a216c0c
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-31 00:24:24 +0500

    Интерфейс сервиса заказов

---
+++ WebShop/Infrastructure/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebShop.Domain.Entities.Orders;
+using WebShop.ViewModels;
+
+namespace WebShop.Infrastructure.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetUserOrders(string UserName);
+
+        Task<Order> GetOrderById(int id);
+
+        Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel);
+    }
+}
