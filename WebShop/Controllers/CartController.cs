cb451b66661bb51914b4a30609e0dec5d93f9b26
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-07-31 00:34:10 +0500

    Добавление логики формирования заказа корзины и представления для этого

--- WebShop/Controllers/CartController.cs
+++ WebShop/Controllers/CartController.cs
@@ -1,40 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebShop.Infrastructure.Interfaces;
+using WebShop.ViewModels;
 
 namespace WebShop.Controllers
 {
     public class CartController : Controller
     {
-        private readonly ICartService _cartService;
+        private readonly ICartService _CartService;
 
-        public CartController(ICartService cartService) => _cartService = cartService;
+        public CartController(ICartService CartService) => _CartService = CartService;
 
-        public IActionResult Details() => View(_cartService.TransformFromCart());
+        public IActionResult Details() => View(new CartOrderViewModel { Cart = _CartService.TransformFromCart() });
 
         public IActionResult AddToCart(int id)
         {
-            _cartService.AddToCart(id);
-            return RedirectToAction(nameof(Details));
-        }
-        public IActionResult DecrementFromCart(int id)
-        {
-            _cartService.DecrementFromCart(id);
-            return RedirectToAction(nameof(Details));
-        }
-        public IActionResult RemoveFromCart(int id)
-        {
-            _cartService.RemoveFromCart(id);
-            return RedirectToAction(nameof(Details));
-        }
-        public IActionResult Clear(int id)
-        {
-            _cartService.Clear();
+            _CartService.AddToCart(id);
             return RedirectToAction(nameof(Details));
         }
 
+        public IActionResult DecrementFromCart(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult Clear()
+        {
+            _CartService.Clear();
+            return RedirectToAction(nameof(Details));
+        }
+
+        public async Task<IActionResult> CheckOut(OrderViewModel OrderModel, [FromServices] IOrderService OrderService)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Details), new CartOrderViewModel
+                {
+                    Cart = _CartService.TransformFromCart(),
+                    Order = OrderModel
+                });
+
+            var order = await OrderService.CreateOrder(User.Identity.Name, _CartService.TransformFromCart(), OrderModel);
+
+            _CartService.Clear();
+
+            return RedirectToAction(nameof(OrderConfirmed), new { id = order.Id });
+        }
+
+        public IActionResult OrderConfirmed(int id)
+        {
+            ViewBag.OrderId = id;
+            return View();
+        }
     }
 }
