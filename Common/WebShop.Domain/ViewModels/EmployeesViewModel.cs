3a4446e7ecdb67d72650cd2abc18b0b2af3938c7
Ivan Kuzminykh <kuzminyh.ivan@gmail.com>
2020-08-01 14:55:12 +0500

    Рефакторинг

---
+++ Common/WebShop.Domain/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя является обязательным полем")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина строки должна быть не более 200 и не менее 3")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата имени")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Фамилия является обязательным полем")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина строки должна быть не более 200 и не менее 3")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина строки должна быть не более 200 и не менее 3")]
+        public string Patronymic { get; set; }
+
+        [Display(Name = "Возраст")]
+        [Required(ErrorMessage = "Возраст является обязательным полем")]
+        [Range(18, 80, ErrorMessage = "Возраст должен быть от 18 до 80")]
+        public int Age { get; set; }
+
+        [Display(Name = "Дата начала трудового договора")]
+        [DataType(DataType.DateTime)]
+        public DateTime EmployementDate { get; set; }
+    }
+}
---
+++ Common/WebShop.Domain/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
